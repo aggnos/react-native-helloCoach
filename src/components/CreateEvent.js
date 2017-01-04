55060dc6734a916cd5aea3789c27dfdec013837e
aggnos <dotonkovi@gmail.com>
2017-01-04 10:42:04 +0100

    eventtype

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput, TouchableHighlight } from 'react-native';
+import {
+    Picker,
+    Text,
+    View,
+    TextInput,
+    TouchableHighlight
+} from 'react-native';
 
 export default class CreateEvent extends Component {
     constructor(props) {
         super(props);
-	console.log("these are my props: ", props)
-
     }
 
     render() {
-	const {increment, eventsState} = this.props; 
-	
+	const {increment, eventsState, eventPickerAction, selectedValue} = this.props; 
+	//     <Picker
+	// selectedValue={this.state.language}
+	// onValueChange={(lang) => this.setState({language: lang})}>
+	//     <Picker.Item label="Java" value="java" />
+	//     <Picker.Item label="JavaScript" value="js" />
+	//     </Picker>
         return (
 		<View>
-	        <TextInput />
+	        <Picker
+	    selectedValue={eventsState.category}
+	    onValueChange={(selectedValue) => eventPickerAction(selectedValue)}
+		>
+		<Picker.Item label="Game" value="game" />
+		<Picker.Item label="Training" value="training" />
+		<Picker.Item label="Meeting" value="meeting" />
+		</Picker>
+
 		<TouchableHighlight onPress={increment}>
-		<Text> MEDELINA{eventsState.event}   </Text>
+		<Text> MEDELINA{eventsState.category}   </Text>
 		</TouchableHighlight>
-		<TouchableHighlight onPress={() => console.log()}>
+		<TouchableHighlight onPress={() => console.log(eventsState.category)}>
 		<Text> SEC BUTTON </Text>
 		</TouchableHighlight>
 		</View>
